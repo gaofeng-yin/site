5bd9586386127e1d0e09cf5ec7c51c354b56335b
gaofeng <gaofengyin@hotmail.com>
2020-10-09 17:03:42 +0100

    contact icon hover

--- src/components/Proj.js
+++ src/components/Proj.js
@@ -14,38 +14,44 @@ function Proj(){
             title: 'Puppet to Ansible compiler',
             img: eli,
             description: 'Compiler which translate Puppet module to Ansible role. Built using Eli: Translator Construction Made Easy. Used to facilitate migration from Puppet to Ansible.',
-            link: 'https://github.com/gaofeng-yin/Puppet2Ansible'
+            link: 'https://github.com/gaofeng-yin/Puppet2Ansible',
+            tags: ["Ansible ", "Puppet"]
         },
         {
             title: 'Budget app',
             img: budgetapp,
             description: 'Budget calculator built using Angular. A small boost to getting started with a basic Angular application. This app make a list of incomes and expenses by adding positive or negative values and then calculate the difference between income and expense. You can also edit de values and discription, consonant of the change, the list get update automatically.',
-            link: 'https://github.com/gaofeng-yin/budget_app'
+            link: 'https://github.com/gaofeng-yin/budget_app',
+            tags: []
         },
         {
             title: 'Weather app',
             img: weather,
             description: 'Weather app created using vue.js framework. In this app you can find weather and temperature of any country or city. App return data from weather api. ',
-            link: 'https://github.com/gaofeng-yin/weather-app'
+            link: 'https://github.com/gaofeng-yin/weather-app',
+            tags: []
         },
         {
             title: 'Portfolio website',
             img: website,
             description: 'Portfolio website, built from scratch using React.',
-            link: 'https://github.com/gaofeng-yin/site'
+            link: 'https://github.com/gaofeng-yin/site',
+            tags: []
         },
         {
             title: 'Json to html table',
             img: parser,
             description: 'Compiler built using YACC&LEX which translate json file, containing Google calendar info, into HTML table cotent. This may help to better visualization of the data.',
-            link: 'https://github.com/gaofeng-yin/language_processor'
+            link: 'https://github.com/gaofeng-yin/language_processor',
+            tags: []
 
         },
         {
             title: 'ROS turtlesim teleop',
             img: ros,
             description: 'Code in Python, developed for ROS to teleoperate ground robots using keyboard (in this case a simulation robot). The Robot Operating System (ROS) is a flexible framework for writing robot software. It is a collection of tools, libraries, and conventions that aim to simplify the task of creating complex and robust robot behavior across a wide variety of robotic platforms.',
-            link: 'https://github.com/gaofeng-yin/ros_turtlesim_teleop'
+            link: 'https://github.com/gaofeng-yin/ros_turtlesim_teleop',
+            tags: ["Ros"]
 
         }
     ]
@@ -57,6 +63,7 @@ function Proj(){
             <h1>{projetos.title}</h1>
             <img className="site-image" src={projetos.img} alt="img" />
             <p><br></br>{projetos.description}</p>
+            <p>Keywords: {projetos.tags}</p>
             <a href={projetos.link} target="_blank" rel="noopener noreferrer">
             <img className="link-proj" src={giticon} alt="img" /></a>
         </div> 
